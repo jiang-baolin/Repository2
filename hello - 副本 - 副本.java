15e67efb07cba2fd788e7bdd166beccb428350b6
jbl <jbl@.com>
2020-03-19 17:24:02 +0800

    新增内容

--- hello - 副本 - 副本.java
+++ hello - 副本 - 副本.java
@@ -2,6 +2,9 @@ class HelloGit{
 	public static void main(String[] args){
 		System.out.println("HELLO GIT")	;
 		System.out.println("one");
+		
+		System.out.println("one");
+		System.out.println("one");
 
 	}
 }
